b8009d6fa639268292add7bae9b5624bab9fe34c
wangmaoquan <1782976211@qq.com>
2022-08-31 17:39:55 +0800

    fix(tokenizer): 修改tokenizer测试描述

--- src/tokenizer/index.spec.ts
+++ src/tokenizer/index.spec.ts
@@ -1,66 +1,68 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 import { tokenizer, TokenTypes } from '.';
 
-test('tokenizer', () => {
-  const code = `(add 2 (subtract 4 2))`;
-
-  const tokens = [
-    { type: TokenTypes.Paren, value: '(' },
-    { type: TokenTypes.Name, value: 'add' },
-    { type: TokenTypes.Number, value: '2' },
-    { type: TokenTypes.Paren, value: '(' },
-    { type: TokenTypes.Name, value: 'subtract' },
-    { type: TokenTypes.Number, value: '4' },
-    { type: TokenTypes.Number, value: '2' },
-    { type: TokenTypes.Paren, value: ')' },
-    { type: TokenTypes.Paren, value: ')' },
-  ];
-
-  expect(tokenizer(code)).toEqual(tokens);
-});
-
-test('left paren', () => {
-  const code = `(`;
-  const tokens = [
-    { type: TokenTypes.Paren, value: '(' },
-  ];
-  expect(tokenizer(code)).toEqual(tokens);
-});
-
-test('right paren', () => {
-  const code = `)`;
-  const tokens = [
-    { type: TokenTypes.Paren, value: ')' },
-  ];
-  expect(tokenizer(code)).toEqual(tokens);
-});
-
-test('name', () => {
-  const code = `add`;
-  const tokens = [
-    { type: TokenTypes.Name, value: 'add' },
-  ];
-  expect(tokenizer(code)).toEqual(tokens);
-});
-
-test('number', () => {
-  const code = `22`;
-  const tokens = [
-    { type: TokenTypes.Number, value: '22' },
-  ];
-  expect(tokenizer(code)).toEqual(tokens);
-});
-
-test('(add 1 2)', () => {
-  const code = `(add 1 2)`;
-
-  const tokens = [
-    { type:  TokenTypes.Paren, value: '(' },
-    { type: TokenTypes.Name, value: 'add' },
-    { type: TokenTypes.Number, value: '1' },
-    { type: TokenTypes.Number, value: '2' },
-    { type: TokenTypes.Paren, value: ')' },
-  ];
-
-  expect(tokenizer(code)).toEqual(tokens);
-});
+describe('tokenizer', () => {
+  test('tokenizer', () => {
+    const code = `(add 2 (subtract 4 2))`;
+  
+    const tokens = [
+      { type: TokenTypes.Paren, value: '(' },
+      { type: TokenTypes.Name, value: 'add' },
+      { type: TokenTypes.Number, value: '2' },
+      { type: TokenTypes.Paren, value: '(' },
+      { type: TokenTypes.Name, value: 'subtract' },
+      { type: TokenTypes.Number, value: '4' },
+      { type: TokenTypes.Number, value: '2' },
+      { type: TokenTypes.Paren, value: ')' },
+      { type: TokenTypes.Paren, value: ')' },
+    ];
+  
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+  
+  test('left paren', () => {
+    const code = `(`;
+    const tokens = [
+      { type: TokenTypes.Paren, value: '(' },
+    ];
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+  
+  test('right paren', () => {
+    const code = `)`;
+    const tokens = [
+      { type: TokenTypes.Paren, value: ')' },
+    ];
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+  
+  test('name', () => {
+    const code = `add`;
+    const tokens = [
+      { type: TokenTypes.Name, value: 'add' },
+    ];
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+  
+  test('number', () => {
+    const code = `22`;
+    const tokens = [
+      { type: TokenTypes.Number, value: '22' },
+    ];
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+  
+  test('(add 1 2)', () => {
+    const code = `(add 1 2)`;
+  
+    const tokens = [
+      { type:  TokenTypes.Paren, value: '(' },
+      { type: TokenTypes.Name, value: 'add' },
+      { type: TokenTypes.Number, value: '1' },
+      { type: TokenTypes.Number, value: '2' },
+      { type: TokenTypes.Paren, value: ')' },
+    ];
+  
+    expect(tokenizer(code)).toEqual(tokens);
+  });
+})
